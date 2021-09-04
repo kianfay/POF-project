65953e0352ef84a8e3d717326627924841e6db49
kianfay <kifay@tcd.ie>
2021-09-04 12:39:31 +0100

    getting custom data in tx

--- addingnextblocktx.py
+++ addingnextblocktx.py
@@ -20,28 +20,44 @@ methodAndParams = [
 address = None;
 for i in range(0,4):
     ret = instruct_wallet(methodAndParams[i][0], methodAndParams[i][1])
-    print(ret)
+    print(ret,'\n')
     if(i == 3):
         address = ret['result']
 
-ret = instruct_wallet("generatetoaddress", [101, address])
+method = 'generatetoaddress';
+ret = instruct_wallet(method, [101, address])
 firstBlockHash = ret['result'][0]
-print(ret)
+print(str(method),':\n', ret,'\n')
 
-ret = instruct_wallet('getblock', [firstBlockHash])
+method = 'getblock';
+ret = instruct_wallet(method, [firstBlockHash])
 txHex = ret['result']['tx'][0]
-print(ret)
+print(str(method),':\n', ret,'\n')
 
-ret = instruct_wallet('createrawtransaction', [
+method = 'createrawtransaction';
+ret = instruct_wallet(method, [
     [{"txid":txHex, "vout":0}], 
     [{"data":"00010203"},{address:"0.01"}]
 ])
 txHex = ret['result']
-print(ret)
+print(str(method),':\n', ret,'\n')
 
-ret = instruct_wallet('signrawtransactionwithwallet', [txHex])
+method = 'signrawtransactionwithwallet';
+ret = instruct_wallet(method, [txHex])
 signedTx = ret['result']['hex']
-print(ret)
+print(str(method),':\n', ret,'\n')
 
-ret = instruct_wallet('generateblock', [address, [signedTx]])
-print(ret)
+method = 'generateblock'
+ret = instruct_wallet(method, [address, [signedTx]])
+newBlockAddr = ret['result']['hash']
+print(method,':\n', ret,'\n')
+
+method = 'getblock'
+ret = instruct_wallet(method, [newBlockAddr])
+newTxHex = ret['result']['tx'][1]
+print(method,':\n', ret,'\n')
+
+method = 'getrawtransaction'
+ret = instruct_wallet(method, [newTxHex, True, newBlockAddr])
+txHex = ret['result']
+print(method,':\n', ret,'\n')
